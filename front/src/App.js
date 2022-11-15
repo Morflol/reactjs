524951beb6786e1143ca1cd1351d62a6374fa87b
Alexis Lamour <alexis@carjager.com>
2022-11-15 21:52:06 +0100

    Hey this is my first commit.

--- front/src/App.js
+++ front/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here Alexis did some changes.
         </p>
         <a
           className="App-link"
